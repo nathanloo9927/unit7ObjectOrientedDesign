fd1d3b4242571329502af8f4c43294d852195f37
nathanloo9927 <ngloo@NN123-HA-04.HS.sd203.org>
2016-02-10 14:24:27 -0600

    20

--- Chapter 10 Practice/ButtonViewer.java
+++ Chapter 10 Practice/ButtonViewer.java
@@ -1,21 +1,53 @@
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.*;
 import java.util.*;
 public class ButtonViewer
 {
-    private static final int FRAME_WIDTH = 100;
-    private static final int FRAME_HEIGHT = 60;
-    public static void main(String[] args)
+    private final int FRAME_WIDTH = 100;
+    private final int FRAME_HEIGHT = 100;
+    private JFrame frame;
+    private JPanel panel;
+    private JButton button;
+    private JButton b;
+    private JLabel label;
+    public ButtonViewer()
     {
-        JFrame frame = new JFrame();
-        JButton button = new JButton("Click Me!");
-        frame.add(button);
+        this.frame = new JFrame();
+        this.panel = new JPanel();
+        
+        this.button = new JButton("For Justice!");
+        this.panel.add(this.button);
+        
+        this.b = new JButton("By the light!");
+        this.panel.add(this.b);
+        
+        this.label = new JLabel();
+        this.panel.add(this.label);
+        
+        this.frame.add(panel);
         
         ClickListener listener = new ClickListener();
-        button.addActionListener(listener);
+        this.button.addActionListener(listener);
         
-        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        ClickListener l = new ClickListener();
+        this.b.addActionListener(l);
+        
+        this.frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setVisible(true);
+    }
+    public static void main(String[] args)
+    {
+        ButtonViewer view = new ButtonViewer();
+    }
+    
+    public class ClickListener implements ActionListener
+    {
+        private int count = 0;
+        public void actionPerformed(ActionEvent event)
+        {
+            count++;
+            label.setText(event.getActionCommand() + " was clicked " + count + " times");
+        }
     }
 }
