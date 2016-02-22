516dbf0e815b36573a3d6e9be3b61100d86a06d5
nathanloo9927 <ngloo@NN123-HA-04.HS.sd203.org>
2016-02-22 14:13:39 -0600

    25

--- Chapter 10 Practice/TriangleComponent.java
+++ Chapter 10 Practice/TriangleComponent.java
@@ -1,35 +1,44 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.geom.*;
 public class TriangleComponent extends JComponent
 {
     private int count;
-    private Rectangle[] point;
+    private Point2D[] point;
+    private static final int max = 3;
     public TriangleComponent()
     {
         count = 0;
-        point = new Rectangle[3];
+        point = new Point2D[max];
     }
     public void addPoint(int x, int y)
     {
-        if (count < 3)
+        if (count < max)
         {
-            point[count] = new Rectangle(x,y,0,0);
+            point[count] = new Point2D.Double(x,y);
             count++;
         } else
         {
             count = 0;
         }
+        this.repaint();
     }
     public void paintComponent(Graphics g)
     {
+        super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        if (count == 0)
+        if (count > 0)
         {
-            g2.draw(point[0]);
-        } else if (count == 1)
+            //g2.draw(new Ellipse2D.Double(this.point[0].getX(), this.point[0].getY()));
+        }
+        if (count > 1)
         {
-            g2.draw(point[1]);
-            g2.drawLine((int)point[0].getX(),(int)point[0].getY(), (int)point[1].getX(), (int)point[1].getY());
+            g2.draw(new Line2D.Double(this.point[0], this.point[1]));
+        }
+        if (count > 2)
+        {
+            g2.draw(new Line2D.Double(this.point[1], this.point[2]));
+            g2.draw(new Line2D.Double(this.point[2], this.point[0]));
         }
     }
     
