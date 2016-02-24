3e508fe6a6a7c3ef857b2b3fa9fc4efe7056492e
nathanloo9927 <ngloo@NN123-HA-04.HS.sd203.org>
2016-02-24 14:13:15 -0600

    27 (Drawing editor lab day2)

--- Drawing Editor/ControlPanel.java
+++ Drawing Editor/ControlPanel.java
@@ -3,23 +3,28 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.geom.*;
 import java.awt.event.*;
+import java.applet.*;
 public class ControlPanel extends JPanel
 {
-    private JPanel panel;
     private JButton color;
     private JButton circle;
     private JButton square;
+    private JPanel bottomPanel;
+    private BorderLayout border;
     public ControlPanel(/*DrawingPanel canvas*/)
     {
-        this.panel = new JPanel();
-        
         this.color = new JButton("Pick Color");
-        this.panel.add(this.color);
         this.circle = new JButton("Add Circle");
         this.square = new JButton("Add Square");
-        this.panel.add(this.circle);
-        this.panel.add(this.square);
         
+        this.bottomPanel = new JPanel();
+        bottomPanel.add(this.color);
+        bottomPanel.add(this.circle);
+        bottomPanel.add(this.square);
+        
+        this.border = new BorderLayout();
+        this.setLayout(border);
+        this.add(bottomPanel, border.SOUTH);
         
     }
 }
--- Drawing Editor/DrawingEditor.java
+++ Drawing Editor/DrawingEditor.java
@@ -5,21 +5,19 @@ import java.awt.geom.*;
 import java.awt.event.*;
 public class DrawingEditor extends JFrame
 {
-    private JFrame frame;
     private final int width = 500;
     private final int height = 500;
     public DrawingEditor()
     {
-        this.frame = new JFrame();
-        
-        this.frame.setTitle("Drawing Editor");
-        this.frame.setSize(width, height);
-        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.frame.setVisible(true);
+        this.setTitle("Drawing Editor");
+        this.setSize(width, height);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         //DrawingPanel canvas = new DrawingPanel();
         ControlPanel controls = new ControlPanel(/*canvas*/);
-        this.frame.add(controls);
+        this.add(controls);
+        
+        this.setVisible(true);
     }
     public static void main(String[] args)
     {
